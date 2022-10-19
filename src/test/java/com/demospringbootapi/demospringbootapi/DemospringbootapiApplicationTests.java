e473acb7c41f82c11e3b68152be82c826f46c283
Yogesh R01 <Yogesh_R01@ad.infosys.com>
2022-10-19 16:56:59 +0530

    first

---
+++ src/test/java/com/demospringbootapi/demospringbootapi/DemospringbootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demospringbootapi.demospringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringbootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
